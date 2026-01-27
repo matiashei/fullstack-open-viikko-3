5c8b6fa1545f454b5bec8a32a90b66cfa93d9f57
Matias Heino <matias.heino@hotmail.com>
2026-01-27 20:34:34 +0200

    3.18

--- index.js
+++ index.js
@@ -22,31 +22,10 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    }
-]
-
 app.get('/info', (request, response) => {
-    response.send(`<div>Phonebook has info for ${persons.length} people<br>${new Date()}</div>`)
+    Person.find({}).then(persons => {
+        response.send(`<div>Phonebook has info for ${persons.length} people<br>${new Date()}</div>`)
+    })
 })
 
 app.get('/api/persons', (request, response) => {
